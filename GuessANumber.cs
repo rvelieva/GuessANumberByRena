f73dab82461e18d13109a8431142999e7d163a63
ZV <ZV@DESKTOP-L1ULQD0>
2024-01-21 15:39:36 +0200

    Upgrades

--- GuessANumber.cs
+++ GuessANumber.cs
@@ -6,8 +6,11 @@
         {
            Random randomNum = new Random();
             int computerNum = randomNum.Next(1, 101);
-            while(true)
+            Console.WriteLine("LEVEL 1");
+            Console.WriteLine();
+            while (true)
             {
+                
                 Console.Write("Guess a number(1-100):");
                 int playerInput = int.Parse(Console.ReadLine());
 
@@ -25,6 +28,58 @@
                 }
             }
             Console.WriteLine("You guessed it!");
+
+            Random randomNumLevel2 = new Random();
+            int computerNumLevel2 = randomNumLevel2.Next(1, 201);
+            Console.WriteLine("LEVEL 2");
+            Console.WriteLine();
+            while (true)
+            {
+                
+                Console.Write("Guess a number(1-200):");
+                int playerInput = int.Parse(Console.ReadLine());
+
+                if (playerInput == computerNumLevel2)
+                {
+                    break;
+                }
+                else if (playerInput > computerNumLevel2)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine("Too low");
+                }
+            }
+            Console.WriteLine("You guessed it!");
+
+            Random randomNumLevel3 = new Random();
+            int computerNumLevel3 = randomNumLevel2.Next(1, 301);
+            Console.WriteLine("LEVEL 3");
+            Console.WriteLine();
+            while (true)
+            {
+                
+                Console.Write("Guess a number(1-300):");
+                int playerInput = int.Parse(Console.ReadLine());
+
+                if (playerInput == computerNumLevel3)
+                {
+                    break;
+                }
+                else if (playerInput > computerNumLevel3)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine("Too low");
+                }
+            }
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine();
+            Console.WriteLine("You win the computer!");
         }
     }
 }
