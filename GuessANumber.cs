d21bfcc556edc3abb2dcc757dff43c218a4eb581
ZV <ZV@DESKTOP-L1ULQD0>
2024-01-21 15:23:14 +0200

    Uploading Guess a number project

---
+++ GuessANumber.cs
@@ -0,0 +1,30 @@
+﻿namespace GuessANumber
+{
+    internal class GuessANumber
+    {
+        static void Main(string[] args)
+        {
+           Random randomNum = new Random();
+            int computerNum = randomNum.Next(1, 101);
+            while(true)
+            {
+                Console.Write("Guess a number(1-100):");
+                int playerInput = int.Parse(Console.ReadLine());
+
+                if(playerInput == computerNum) 
+                {
+                    break;
+                }
+                else if (playerInput> computerNum) 
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine("Too low");
+                }
+            }
+            Console.WriteLine("You guessed it!");
+        }
+    }
+}
